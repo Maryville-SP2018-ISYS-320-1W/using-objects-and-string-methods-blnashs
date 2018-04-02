357f564f6d27c59c6bab912aa46314e07fdc395c
Barbara Nash <bnash1@live.maryville.edu>
2018-04-01 20:14:56 -0500

    string 96

--- src/P4_Vertical.java
+++ src/P4_Vertical.java
@@ -1,13 +1,22 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Barbara Nash
+	Date: April 1, 2018
 */
 
 public class P4_Vertical {
 
 	public static void main(String[] args) {
-
+		//Function call to invoke the method vertical("hey now");}
+/*This function accepts a string as its parameter
+ * and prints each letter of the string
+ * on separate lines.*/
+	public static void vertical (String str))
+	system.out.println("The call of vertical (\"" +strt"\") produce the followin output:");
+	for (int i=o;i<str.length(); i++)
+	{
+		//Display the output
+		Syste.out.println(str.charAt(i));
 	}
-
+ */
 }
