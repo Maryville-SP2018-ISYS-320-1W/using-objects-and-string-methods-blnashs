89f51d52bd4fb45a51979561df131b03e64cecaa
Barbara Nash <bnash1@live.maryville.edu>
2018-04-01 19:55:14 -0500

    string 95

--- src/P1_TestStringMethods.java
+++ src/P1_TestStringMethods.java
@@ -7,19 +7,19 @@
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. number of characters in the string 12
+ 2. characters at a specific index (a)
+ 3. characters at a specific index ((g) 
+ 4. index of particular character (
  
- 5.
- 6.
- 7.
- 8.
+ 5.  a new string with all uppercase GANDALF THE GRAY
+ 6.  a new string in all lowercase gandalf the gray
+ 7.  characters from start index (o)
+ 8.  characters from start to end (dalf the gray)
  
- 9.
- 10.
- 11.
+ 9. Goondoolf the grooy
+ 10. Gandalf the White
+ 11.  Frangeodo Baggins
  
   
  */
@@ -27,24 +27,26 @@
 /*
  Were you correct? Explain if you were not (what did you learn?)
  
- 1.
- 2.
- 3. 
- 4.
+ 1. didnt count the space
+ 2. got it
+ 3. got it
+ 4.  missed.
  
- 5.
- 6.
- 7.
- 8.
+ 5. got it
+ 6. used wrong string
+ 7. only used the letter at 4 didnt complete through
+ 8. ran through too many places
  
- 9.
- 10.
- 11.
+ 9. put oo on last word as well
+ 10.I replaced gray
+ 11.  I missed the " around str1
  
  */
 public class P1_TestStringMethods {
 	public static void main(String[] args) {
-		System.out.println("REPLACE WITH YOUR EXPRESSION");
-	}
+		String str1 = "Frodo Baggins";
+		String str2 = "Gandalf the GRAY";
+		System.out.println("str1".replace("r",  "range"));
 
+}
 }
